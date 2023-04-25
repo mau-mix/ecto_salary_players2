4a5c66c185d09ac71339da8362654ae81f5a3656
Mauricio <m.musicalmix@gmail.com>
2023-04-24 21:39:15 -0600

    creando la primera migracion

--- priv/repo/migrations/20230425033518_create_players.exs
+++ priv/repo/migrations/20230425033518_create_players.exs
@@ -2,6 +2,17 @@ defmodule Players.Repo.Migrations.CreatePlayers do
   use Ecto.Migration
 
   def change do
+    create table(:players) do
+      add :name, :string
+      add  :level, :string
+      add  :goals, :integer
+      add :salary, :integer
+      add  :bond, :integer
+      add  :full_salary, :integer
+      add  :team, :string
 
+      timestamps()
+
+    end
   end
 end
