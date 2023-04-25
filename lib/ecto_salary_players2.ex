2b8653a85c7c9870127ce8586675e0edec6ddfbf
Mauricio <m.musicalmix@gmail.com>
2023-04-24 21:14:29 -0600

    iniciando proyecto e instalando dependencias

---
+++ lib/ecto_salary_players2.ex
@@ -0,0 +1,18 @@
+defmodule EctoSalaryPlayers2 do
+  @moduledoc """
+  Documentation for `EctoSalaryPlayers2`.
+  """
+
+  @doc """
+  Hello world.
+
+  ## Examples
+
+      iex> EctoSalaryPlayers2.hello()
+      :world
+
+  """
+  def hello do
+    :world
+  end
+end
---
+++ test/ecto_salary_players2_test.exs
@@ -0,0 +1,8 @@
+defmodule EctoSalaryPlayers2Test do
+  use ExUnit.Case
+  doctest EctoSalaryPlayers2
+
+  test "greets the world" do
+    assert EctoSalaryPlayers2.hello() == :world
+  end
+end
